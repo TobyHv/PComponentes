c38a0c0874e3a2fde7aeba1ffd7642812e6ccfb9
TobyHv <andresjosue@hotmail.es>
2025-05-08 15:43:19 -0600

    Puede

--- config/filesystems.php
+++ config/filesystems.php
@@ -20,22 +20,6 @@ return [
             'throw' => false,
         ],
 
-        // Si no usas S3, puedes comentar o eliminar esta sección.
-        // Aquí la dejamos comentada para evitar las advertencias:
-        /*
-        's3' => [
-            'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID', ''),
-            'secret' => env('AWS_SECRET_ACCESS_KEY', ''),
-            'region' => env('AWS_DEFAULT_REGION', ''),
-            'bucket' => env('AWS_BUCKET', ''),
-            'url' => env('AWS_URL', ''),
-            'endpoint' => env('AWS_ENDPOINT', ''),
-            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-            'throw' => false,
-        ],
-        */
-
     ],
 
     'links' => [
